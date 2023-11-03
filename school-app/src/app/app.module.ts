b90ac9a8c2a9f2a8d821ecf159ea0e1139f38a39
Angel Iliev <ailievengineer@gmail.com>
2023-11-03 20:54:44 +0200

    Added new components

--- school-app/src/app/app.module.ts
+++ school-app/src/app/app.module.ts
@@ -4,11 +4,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JitsiComponent } from './jitsi/jitsi.component';
+import { AbsenceComponent } from './absence/absence.component';
+import { AdminComponent } from './admin/admin.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ChatComponent } from './chat/chat.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { FooterComponent } from './footer/footer.component';
+import { ForumComponent } from './forum/forum.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NotificationComponent } from './notification/notification.component';
+import { ParentComponent } from './parent/parent.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { StudentComponent } from './student/student.component';
+import { SubjectComponent } from './subject/subject.component';
+import { TeacherComponent } from './teacher/teacher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JitsiComponent
+    JitsiComponent,
+    AbsenceComponent,
+    AdminComponent,
+    CalendarComponent,
+    ChatComponent,
+    FeedbackComponent,
+    FooterComponent,
+    ForumComponent,
+    HeaderComponent,
+    LoginComponent,
+    NavigationComponent,
+    NotificationComponent,
+    ParentComponent,
+    ProfileComponent,
+    RegisterComponent,
+    StudentComponent,
+    SubjectComponent,
+    TeacherComponent
   ],
   imports: [
     BrowserModule,
