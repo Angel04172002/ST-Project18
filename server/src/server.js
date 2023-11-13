57b1f85a52559ac7c6f31c2bb70cb5e23ea3d367
Boyan Kovachev <boyankovachev40@gmail.com>
2023-11-13 22:35:52 +0200

    Added cors

--- server/src/server.js
+++ server/src/server.js
@@ -13,6 +13,11 @@ app.use(
     extended: true,
   })
 )
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+    next();
+});
 
 const routes = require("./routes/routes");
 app.use('/', routes)
