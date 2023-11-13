01b3ecede731ef9329531d102ea554dc3d7c58c8
Angel Iliev <ailievengineer@gmail.com>
2023-11-13 13:17:51 +0200

    Добавих таблицата за оценки

--- school-app/src/app/absence/absence.component.ts
+++ school-app/src/app/absence/absence.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-absence',
   templateUrl: './absence.component.html',
-  styleUrls: ['./absence.component.css']
+  styleUrls: ['./absence.component.css', '../styles/table-style.css']
 })
 export class AbsenceComponent {
 
--- school-app/src/app/grades/grades.component.ts
+++ school-app/src/app/grades/grades.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-grades',
   templateUrl: './grades.component.html',
-  styleUrls: ['./grades.component.css']
+  styleUrls: ['./grades.component.css', '../styles/table-style.css']
 })
 export class GradesComponent {
 
