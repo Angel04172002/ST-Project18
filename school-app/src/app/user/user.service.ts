e33bc1ab1497ec08496f1630ba96073c749bb8cf
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2023-11-15 14:25:37 +0200

    Update user.service.ts

--- school-app/src/app/user/user.service.ts
+++ school-app/src/app/user/user.service.ts
@@ -3,13 +3,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Profile } from '../types/Profile';
 import { Observable } from 'rxjs';
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3000';
+const emailPattern = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+const passwordPattern = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{10,}$');
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+
+  headers = new HttpHeaders({
+    'content-type': 'application/json'
+  });
 
 
 
@@ -40,14 +46,45 @@ export class UserService {
     };
     //TODO: Validations 
 
-    this.http.post(`${BASE_URL}/profile/auth`, user, { headers })
+    this.http.post(`${BASE_URL}/profile/auth`, user, { headers: this.headers })
       .subscribe(data => console.log(data));
-      
+
+  }
+
+
+
+
+
+  register(firstName: string, lastName: string, email: string, password: string, type: string) {
+
+
+
+    const user = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      type: type
+    };
+
+    //TODO: Validations 
+
+    this.http.post(`${BASE_URL}/profile/create`, user, { headers: this.headers })
+      .subscribe(data => console.log(data));
+
+  }
+
+
+
+  validateUser(user: Profile): boolean {
+    return Boolean(emailPattern.exec(user.email) && passwordPattern.exec(user.password));
 
   }
 
   logout() {
     localStorage.removeItem('user');
   }
-
 }
+
+
+
