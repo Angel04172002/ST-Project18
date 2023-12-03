7fa6066ab725d9867dd0d0f7dd3578cafccb2c5e
Angel Iliev <ailievengineer@gmail.com>
2023-12-03 12:19:52 +0200

    Small changes in admin routing

--- school-app/src/app/admin/admin-routing.module.ts
+++ school-app/src/app/admin/admin-routing.module.ts
@@ -5,6 +5,9 @@ import { AdminComponent } from "./admin/admin.component";
 import { AdminPanelComponent } from "./admin-panel/admin-panel.component";
 import { AdminSubjectComponent } from "./admin-subject/admin-subject.component";
 import { AdminTeacherComponent } from "./admin-teacher/admin-teacher.component";
+import { CalendarComponent } from "../pages/calendar/calendar.component";
+import { ChatComponent } from "../pages/chat/chat/chat.component";
+import { GroupChatComponent } from "../pages/chat/group-chat/group-chat.component";
 
 const routes : Routes = [
     {
@@ -22,6 +25,18 @@ const routes : Routes = [
     {
         path: 'admin-students',
         component: AdminComponent
+    },
+    {
+        path: 'calendar',
+        component: CalendarComponent
+    },
+    {
+        path: 'chat',
+        component: ChatComponent
+    },
+    {
+        path: 'group-chat',
+        component: GroupChatComponent
     }
 ]
   
--- server/src/database/teacher.queries.js
+++ server/src/database/teacher.queries.js
@@ -1,4 +1,8 @@
 
 const addSubjectsAndGradesQuery = `
 
+    
+
 `
+
+module
