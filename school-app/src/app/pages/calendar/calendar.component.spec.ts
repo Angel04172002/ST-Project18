0fe320c681089d789b8a034aab3c101b94c092f3
Zornitsa Stoyanova <zorito_999@abv.bg>
2024-01-09 13:14:24 +0200

    add some basic tests

--- school-app/src/app/pages/calendar/calendar.component.spec.ts
+++ school-app/src/app/pages/calendar/calendar.component.spec.ts
@@ -1,45 +1,21 @@
-const events: { id: any; }[] = [];
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-function addEvent() {
-  const name = document.getElementById("event-name").value;
-  const date = document.getElementById("event-date").value;
-  const time = document.getElementById("event-time").value;
+import { CalendarComponent } from './calendar.component';
 
-  const event = {
-    id: events.length + 1,
-    name,
-    date,
-    time,
-  };
+describe('AbsenceComponent', () => {
+  let component: CalendarComponent;
+  let fixture: ComponentFixture<CalendarComponent>;
 
-  events.push(event);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CalendarComponent]
+    });
+    fixture = TestBed.createComponent(CalendarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  updateCalendar();
-}
-
-function updateCalendar() {
-  const calendarTable = document.querySelector(".calendar table tbody");
-  calendarTable.innerHTML = "";
-  for (const event of events) {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${event.date}</td>
-      <td>${event.name}</td>
-      <td>${event.time}</td>
-      <td>
-        <button onclick="deleteEvent(${event.id})">Изтрий</button>
-      </td>`;
-    calendarTable.appendChild(tr);
-  }
-}
-
-function deleteEvent(id) {
-  for (let i = 0; i < events.length; i++) {
-    if (events[i].id === id) {
-      events.splice(i, 1);
-      break;
-    }
-  }
-
-  updateCalendar();
-}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
